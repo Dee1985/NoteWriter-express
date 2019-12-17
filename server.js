bc87ad139a367eed4c70be8f7d2053b55514a3b4
AyDy Burling <aydy.you@gmail.com>
2019-12-17 01:02:28 -0600

    fixed file path, html and js files linked

--- server.js
+++ server.js
@@ -7,28 +7,31 @@
   // Sets up the Express App
   // =============================================================
   const app = express();
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5500;
+
   
   // Sets up the Express app to handle data parsing
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(express.json());
+ app.use(express.static('public'))
+    // app.use(express.static('public'))- use when there is public folder
   
   // Routes
   // Basic route that sends the user first to the AJAX Page
-    app.get("/", function(req, res) {
-     res.sendFile(path.join(__dirname, "index.html"));  
+    app.get("/notes.html", function(req, res) {
+     res.sendFile(path.join(__dirname, "notes.html"));  
     
     
   });
   
   
-    app.get("/notes", function(req, res) {
-       res.sendFile(path.join(__dirname, "notes.html")); 
+    app.get("*", function(req, res) {
+       res.sendFile(path.join(__dirname, "index.html")); 
      
     });
     
-     app.post("/notes", function(req, res){
-           console.log("hellooo post request")
+     app.post("/notes.html", function(req, res){
+           console.log("hellooo post request") // postman will throw error and not show text, but console log will show in terminal
            
           
     
